f9e72789881e9d1b807cadbe193b62276527fe9c
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-08 00:02:29 +0530

    final changes

--- Project_3_notesApp/script.js
+++ Project_3_notesApp/script.js
@@ -1,6 +1,20 @@
 const notesContainer = document.querySelector(".notes_container");
 const createBtn = document.querySelector(".btn")
 
+function showNotes(){
+    const savedNotes = localStorage.getItem("notes");
+    if (savedNotes) {
+        notesContainer.innerHTML = savedNotes;
+    } else {
+        notesContainer.innerHTML = ""; // Ensure it's clean
+    }
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 let notes= document.querySelectorAll(".input-box")
 
 createBtn.addEventListener("click", ()=>{
@@ -10,4 +24,26 @@ createBtn.addEventListener("click", ()=>{
     inputBox.setAttribute("contenteditable", true);
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName=== "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName==="p"){
+        notes =document.querySelectorAll(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 })
